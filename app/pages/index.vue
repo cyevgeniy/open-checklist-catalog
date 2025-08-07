a5ebcd6f2d7455d5c6a8c2aa0d9a7684b6687a37
Evgeny Chaban <hi@evg.moe>
2025-08-07 09:43:59 +0300

    fix: grid layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -16,9 +16,21 @@ const { data } = await useAsyncData('all', () => {
     grid-row-gap: 16px;
     grid-column-gap: 16px;
     grid-template-rows: auto auto;
-    grid-template-columns: 1fr 1fr;
     grid-auto-columns: 1fr;
-    display: grid
-;
+    display: grid;
+}
+
+@media  screen and (min-width: 640px) {
+  .grid {
+    grid-template-columns: 1fr 1fr;
+
+  }
+}
+
+@media  screen and (min-width: 1024px) {
+  .grid {
+    grid-template-columns: repeat(4, 1fr);
+
+  }
 }
 </style>
