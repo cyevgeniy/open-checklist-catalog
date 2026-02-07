d381a45ccb141bc516869fde5c693ce575771a91
Evgeny Chaban <hi@evg.moe>
2026-02-07 16:11:30 +0300

    chore: add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  modules: ['@nuxt/content', '@nuxt/icon', '@nuxt/eslint'],
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/icon'],
   app: {
     head: {
-            link: [
-              { rel: 'icon', type: 'type="image/svg+xml"', href: '/favicon.svg' },
-              { rel: 'icon', href: '/favicon.ico', sizes: '32x32' },
-            ]
-          }
-  }
+      link: [
+        { rel: 'icon', type: 'type="image/svg+xml"', href: '/favicon.svg' },
+        { rel: 'icon', href: '/favicon.ico', sizes: '32x32' },
+      ],
+    },
+  },
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
